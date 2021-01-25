643e48794a009406eea0015e283417d38a81fd0b
Cíntia Fumi <cintiafumi@gmail.com>
2021-01-25 19:20:41 -0300

    :tada: day 1: react immersion

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../components/Widget';
+import Footer from '../components/Footer';
+import GithubCorner from '../components/GithubCorner';
+import QuizBackground from '../components/QuizBackground';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>The legend of Zelda</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl='http://github.com/cintiafumi' />
+    </QuizBackground>
+  );
 }
